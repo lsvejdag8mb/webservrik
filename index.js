a983009f1b33be91fbcb3c371ddbaec5f890ab1f
svejda <svejda@g8mb.cz>
2022-01-04 15:29:58 +0100

    nacteni souboru

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const http = require("http");
+const fs = require("fs");
 
 function main(req, res) {
-    res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<html><body>Hello HTML!</body></html>");
+    console.log(req.url);
+
+    if (req.url == "/") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end(fs.readFileSync("index.html"));
+    } else if (req.url == "/script.js") {
+        res.writeHead(200, {"Content-type": "text/javascript"});
+        res.end(fs.readFileSync("script.js"));
+    } else if (req.url == "/style.css") {
+        res.writeHead(200, {"Content-type": "text/css"});
+        res.end(fs.readFileSync("style.css"));
+    } else { //not found
+        res.writeHead(404);
+        res.end();
+    }
+
 }
 
 let srv = http.createServer(main);
