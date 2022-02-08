0cfa3c0bbcd0dee2f0560afe4b05b24c1eb0c83f
Libor Švejda <54348957+lsvejdag8mb@users.noreply.github.com>
2022-02-08 14:03:45 +0000

    unikatni id pro pridanou polozku

--- crud.js
+++ crud.js
@@ -13,6 +13,7 @@ exports.crud = function(req, res) {
     res.writeHead(200, { "Content-type": "application/json" });
     let newItem = {};
     // napr. /crud/create?firstname=Test&lastname=Testicek&yob=2002
+    newItem.id = Date.now();
     newItem.firstName = params.firstname;
     newItem.lastName = params.lastname;
     newItem.yob = params.yob;
@@ -22,6 +23,7 @@ exports.crud = function(req, res) {
 
     let obj = {};
     obj.status = "ok";
+    obj.newId = newItem.id;
     obj.count = items.length;
     res.end(JSON.stringify(obj));
   } else if (req.url.startsWith("/crud/read")) {
