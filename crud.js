bdf6d6bdde9e098fc0f51ae9338b2a3d4bf3c86f
Libor Švejda <54348957+lsvejdag8mb@users.noreply.github.com>
2022-02-08 14:20:49 +0000

    /crud/delete

--- crud.js
+++ crud.js
@@ -8,8 +8,9 @@ if (fs.existsSync(FILE_ITEMS)) {
 }
 
 exports.crud = function(req, res) {
+  let params = url.parse(req.url, true).query;
+
   if (req.url.startsWith("/crud/create")) {
-    let params = url.parse(req.url, true).query;
     res.writeHead(200, { "Content-type": "application/json" });
     let newItem = {};
     // napr. /crud/create?firstname=Test&lastname=Testicek&yob=2002
@@ -30,6 +31,23 @@ exports.crud = function(req, res) {
     res.writeHead(200, { "Content-type": "application/json" });
     let obj = {};
     obj.items = items;
+    res.end(JSON.stringify(obj));
+  } else if (req.url.startsWith("/crud/delete")) {
+    res.writeHead(200, { "Content-type": "application/json" });
+
+    let obj = {};
+    obj.status = "error";
+    obj.error = "item not found";
+    for (let i=0; i<items.length; i++) {
+      if (items[i].id == params.id) {
+        items.splice(i, 1);
+        fs.writeFileSync(FILE_ITEMS, JSON.stringify(items));
+        obj.status = "ok";
+        obj.error = undefined;
+        break;
+      }
+    }
+
     res.end(JSON.stringify(obj));
   } else { //not found
     res.writeHead(404);
